9ad33a4648fbcc28f67b66351d869e207620a3e2
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2020-11-12 18:04:50 +0530

    Done

--- src/main/java/com/example/demo/SpringJenkinsApplication.java
+++ src/main/java/com/example/demo/SpringJenkinsApplication.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 	public static void main(String[] args) {
+		logger.info("Application Started");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
--- src/test/java/com/example/demo/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/demo/SpringJenkinsApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+
+
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test Case are executing");
+//		assertEquals(true,true);
 	}
 
 }
